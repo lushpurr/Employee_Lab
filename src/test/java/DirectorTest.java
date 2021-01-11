8e19319a2642970f5c0961f4de97c068f018dfca
lushpurr <lushpurr@gmail.com>
2021-01-11 17:06:47 +0000

    director tests passing

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,39 @@
+import management.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+    Director director;
+
+    @Before
+    public void before(){
+        director = new Director("Susan", "JY36789", 40000,"Management", 200000);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Susan", director.getName());
+    }
+
+    @Test
+    public void canGetnINumber(){
+        assertEquals("JY36789", director.getNiNumber());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(40000, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetDeptName(){
+        assertEquals("Management", director.getDeptName());
+    }
+
+    @Test
+    public void canGetBudget(){
+        assertEquals(200000, director.getBudget(), 0.01);
+    }
+}
